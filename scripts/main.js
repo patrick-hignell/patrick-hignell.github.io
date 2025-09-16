644e5b53ccca1a36b35f4bcc26f1889e8611854d
Patrick Hignell <patrickhignell@hotmail.com>
2025-09-16 16:14:55 +1200

    add main.js, add scriptable sidenav, add neuroplasticity.html

---
+++ scripts/main.js
@@ -0,0 +1,60 @@
+let currentPage = document.getElementsByTagName('title')[0].innerHTML
+let isIndex = false
+
+let navList = [
+  { name: 'Home', link: 'index.html' },
+  { name: 'Te-Houtaewa', link: 'te-houtaewa-template.html' },
+  { name: 'CSS - Display', link: 'html-css.html' },
+  { name: 'Identity & Values', link: 'identity-values.html' },
+  { name: 'Learning Plan', link: 'learning-plan.html' },
+  { name: 'Javascript & the DOM', link: 'javascript-dom.html' },
+  { name: 'Emotional Intelligence', link: 'emotional-intelligence.html' },
+  { name: 'Neuroplasticity', link: 'neuroplasticity.html' },
+]
+
+checkIfIsIndex() // if page is index.html, isIndex = true
+editNavList() // makes changes to links in navList depending if index or blog
+populateSidenav() // adds navList links to sideNav
+
+// ------- Functions ------- //
+
+function checkIfIsIndex() {
+  if (currentPage === "Patrick's Coding Adventure") {
+    isIndex = true
+  }
+}
+
+function editNavList() {
+  if (isIndex === true) {
+    navList.forEach(function (element, index) {
+      if (index !== 0) {
+        element.link = 'blog/' + element.link
+      }
+    })
+  } else {
+    navList[0].link = '../index.html'
+  }
+}
+
+function populateSidenav() {
+  let sidenavParent = document.getElementsByClassName('sidenav')[0]
+  navList.forEach(function (element) {
+    addNewSidenavLink(sidenavParent, element.name, element.link)
+  })
+}
+
+function addNewSidenavLink(fparent, ftext, flink) {
+  let fchild = document.createElement('a')
+  fparent.appendChild(fchild)
+  fchild.innerHTML = ftext
+  fchild.href = flink
+}
+
+//function addNewChild(fparent, ftag, fclass, ftext) {
+//  let fchild = document.createElement(ftag)
+//  fparent.appendChild(fchild)
+//  if (fclass !== false) {
+//    fChild.classList.add(fclass)
+//  }
+//  fchild.innerHTML = ftext
+//}
