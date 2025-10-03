26acc700ca03be87ca2b4cf0b4c0d319f7aaa4ce
Patrick Hignell <patrickhignell@hotmail.com>
2025-10-03 17:52:34 +1300

    remove new line from example 3

--- scripts/personal-project.js
+++ scripts/personal-project.js
@@ -82,7 +82,7 @@ const classArray = [
 const examplesArray = [
   '// If Example-\nlet amIAConjurerOfCheapTricks = false\nif (amIAConjurerOfCheapTricks == false) {\n  console.log("I am not trying to rob you, I am trying to help you")\n} else {\n  console.log("I am not trying to help you, I am trying to rob you")\n} // will log "I am not trying to rob you, I am trying to help you"]',
   '// Switch Example-\nlet stubbornness = 5\nswitch (stubbornness) {\n  case 5:\n    console.log("you shall not pass!")\n    break\n  case 4:\n    console.log("you might not pass!")\n    break\n  case 3:\n    console.log("pass or not pass, I dont care!")\n    break\n  case 2:\n    console.log("you could pass if you wanted to!")\n    break\n  case 1:\n    console.log("I would really love it if you would pass!")\n    break\n  default:\n    console.log("I dont know how I feel about you passing!")\n    break\n} // will log "you shall not pass!"',
-  '// Loop Example-\nlet sentenceStart = "Ho"\nlet sentenceMid = "ld the "\nlet sentenceEnd = "door!"\nfor (let i = 0; i <= sentenceMid.length; i++) {\n  let newSentenceMid = sentenceMid.substring(0, sentenceMid.length - i)\n  console.log(sentenceStart + newSentenceMid + sentenceEnd)\n}\n\n// will log "Hold the door!"\n//          "Hold thedoor!"\n//          "Hold thdoor!"\n//          "Hold tdoor!"\n//          "Hold door!"\n//          "Holddoor!"\n//          "Holdoor!"\n//          "Hodoor!"',
+  '// Loop Example-\nlet sentenceStart = "Ho"\nlet sentenceMid = "ld the "\nlet sentenceEnd = "door!"\nfor (let i = 0; i <= sentenceMid.length; i++) {\n  let newSentenceMid = sentenceMid.substring(0, sentenceMid.length - i)\n  console.log(sentenceStart + newSentenceMid + sentenceEnd)\n}\n// will log "Hold the door!"\n//          "Hold thedoor!"\n//          "Hold thdoor!"\n//          "Hold tdoor!"\n//          "Hold door!"\n//          "Holddoor!"\n//          "Holdoor!"\n//          "Hodoor!"',
   '// Function Example-\nlet spell = "wingardium leviosaar"\nlet featherFloating = false\nfunction checkSpell(testSpell) {\n  if (testSpell == "wingardium leviosa") {\n    featherFloating = true\n  } else {\n    console.log("Its wingardium leviosa, not " + testSpell + "!")\n  }\n}\ncheckSpell(spell) // will log "Its wingardium leviosa, not wingardium leviosaar!"',
 ]
 
@@ -102,8 +102,8 @@ root.style.setProperty('--hover-color', '#ffd90000')
 
 let formText = document.getElementById('formText')
 //customFunctionInput = document.getElementById('customFunctionInput')
-exampleContainer = document.getElementById('exampleContainer')
-htmlContainer = document.getElementById('htmlContainer')
+let exampleContainer = document.getElementById('exampleContainer')
+let htmlContainer = document.getElementById('htmlContainer')
 const buttons = {
   classButton: document.getElementById('classButton'),
   colorButton: document.getElementById('colorButton'),
@@ -112,8 +112,9 @@ const buttons = {
   example3Button: document.getElementById('example3Button'),
   example4Button: document.getElementById('example4Button'),
 }
-colorPicker = document.getElementById('colorPicker')
+let colorPicker = document.getElementById('colorPicker')
 let mouseDisplay = document.getElementById('mouseDisplay')
+
 formText.addEventListener('input', (event) => highlight(event.target.value))
 
 document.addEventListener('mousemove', mouseDisplayMove)
@@ -419,7 +420,7 @@ function spanPressed() {
 }
 
 function spanHovered() {
-  if (clickSetting === 'classButton') {
+  if (clickSetting !== '') {
     mouseDisplay.className = ''
     mouseDisplay.innerHTML = this.className
     mouseDisplay.className = this.className
